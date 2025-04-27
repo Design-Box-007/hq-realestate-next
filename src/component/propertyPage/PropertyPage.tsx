35bfb674698a48ff8cdac8601c6541267c9fc105
Rasmi142 <rasmioff@gmail.com>
2025-04-27 17:38:33 +0530

    error cleared

--- src/component/propertyPage/PropertyPage.tsx
+++ src/component/propertyPage/PropertyPage.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState } from 'react';
+import React  from 'react';
 import HeroSection from '../Common/HeroSection/HeroSection';
 
 import PropertyPortfolio from './PropertyGrid';
